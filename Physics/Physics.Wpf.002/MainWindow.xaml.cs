d3f66f72dfc1b9c614d2aacaf02a5eff5c92adf1
ezszoftver <erdoszoltan256@gmail.com>
2020-04-11 16:11:32 +0200

    + inc. fps

--- Physics/Physics.Wpf.002/MainWindow.xaml.cs
+++ Physics/Physics.Wpf.002/MainWindow.xaml.cs
@@ -209,7 +209,7 @@ namespace Physics.Wpf._002
             GL.MatrixMode(MatrixMode.Modelview);
             GL.LoadMatrix(ref m_modelview);
 
-            int steps = 20;
+            int steps = 50;
             float step = dt / (float)steps;
 
             for (int i = 0; i < steps; i++)
