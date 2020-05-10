2c3b08628e55f4972d8dc0dca4930de555efcc12
ezszoftver <erdoszoltan256@gmail.com>
2020-05-10 11:37:48 +0200

    + optimize, faster. v1

--- Physics/Physics/CollisionDetection.cs
+++ Physics/Physics/CollisionDetection.cs
@@ -12,7 +12,7 @@ namespace Physics
     public class CollisionDetection
     {
         public static int step = 10;
-        public static float margin = 0.005f;
+        public static float margin = 0.01f;
 
         public static bool RigidBodyAndPlane(RigidBody rigidBody, Plane plane, ref List<Hit> listHits, ref Vector3 v3Separate)
         {
@@ -70,114 +70,26 @@ namespace Physics
 
         public static bool RigidBodyAndRigidBody(RigidBody rigidBody1, RigidBody rigidBody2, ref List<Hit> listHits, ref Vector3 v3Separate2) 
         {
-            // aabb swep-test
-            if
-            (
-                false == IsOverlapAABB(rigidBody1, rigidBody2)
-             || false == IsOverlapAABB(rigidBody2, rigidBody1)
-            )
-            {
-                return false;
-            }
-
             // points in other body
-            List<Hit> listHits2 = new List<Hit>();
-            GetPointsInConvexMesh(rigidBody1, rigidBody2, ref listHits2);
-
-            //List<Hit> listHits1 = new List<Hit>();
-            //GetPointsInConvexMesh(rigidBody2, rigidBody1, ref listHits1);
-
-            foreach (Hit hit in listHits2) 
-            {
-                listHits.Add(hit);
-            }
-
-            //foreach (Hit hit in listHits1)
-            //{
-            //    //hit.m_v3Normal *= -1.0f;
-            //    listHits.Add(hit);
-            //}
+            listHits.Clear();
+            GetPointsInConvexMesh(rigidBody1, rigidBody2, ref listHits);
 
             return (listHits.Count() > 0);
-        }
-
-        private static bool IsOverlapAABB(RigidBody rigidBody1, RigidBody rigidBody2) 
-        {
-
-            Matrix4 m4FinalTransform = Matrix4.Mult(rigidBody2.m_m4World, rigidBody1.m_m4World.Inverted());
-            
-            Vector3 v3RigidBody1Min = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
-            Vector3 v3RigidBody1Max = new Vector3(float.MinValue, float.MinValue, float.MinValue);
-
-            foreach (Vector3 v3Point in rigidBody1.m_listPoints) 
-            {
-                v3RigidBody1Min.X = (v3Point.X < v3RigidBody1Min.X) ? (v3Point.X) : (v3RigidBody1Min.X);
-                v3RigidBody1Min.Y = (v3Point.Y < v3RigidBody1Min.Y) ? (v3Point.Y) : (v3RigidBody1Min.Y);
-                v3RigidBody1Min.Z = (v3Point.Z < v3RigidBody1Min.Z) ? (v3Point.Z) : (v3RigidBody1Min.Z);
-
-                v3RigidBody1Max.X = (v3Point.X > v3RigidBody1Max.X) ? (v3Point.X) : (v3RigidBody1Max.X);
-                v3RigidBody1Max.Y = (v3Point.Y > v3RigidBody1Max.Y) ? (v3Point.Y) : (v3RigidBody1Max.Y);
-                v3RigidBody1Max.Z = (v3Point.Z > v3RigidBody1Max.Z) ? (v3Point.Z) : (v3RigidBody1Max.Z);
-            }
-
-            Vector3 v3RigidBody2Min = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
-            Vector3 v3RigidBody2Max = new Vector3(float.MinValue, float.MinValue, float.MinValue);
-
-            foreach (Vector3 v3Point in rigidBody2.m_listPoints) 
-            {
-                Vector3 v3Point2 = Vector4.Transform(new Vector4(v3Point, 1), m4FinalTransform).Xyz;
-
-                v3RigidBody2Min.X = (v3Point2.X < v3RigidBody2Min.X) ? (v3Point2.X) : (v3RigidBody2Min.X);
-                v3RigidBody2Min.Y = (v3Point2.Y < v3RigidBody2Min.Y) ? (v3Point2.Y) : (v3RigidBody2Min.Y);
-                v3RigidBody2Min.Z = (v3Point2.Z < v3RigidBody2Min.Z) ? (v3Point2.Z) : (v3RigidBody2Min.Z);
-                
-                v3RigidBody2Max.X = (v3Point2.X > v3RigidBody2Max.X) ? (v3Point2.X) : (v3RigidBody2Max.X);
-                v3RigidBody2Max.Y = (v3Point2.Y > v3RigidBody2Max.Y) ? (v3Point2.Y) : (v3RigidBody2Max.Y);
-                v3RigidBody2Max.Z = (v3Point2.Z > v3RigidBody2Max.Z) ? (v3Point2.Z) : (v3RigidBody2Max.Z);
-            }
-
-            
-
-            // X
-            bool bCollX = false;
-            float fXMin = Math.Min(v3RigidBody1Min.X, v3RigidBody2Min.X);
-            float fXMax = Math.Max(v3RigidBody1Max.X, v3RigidBody2Max.X);
-            float fFull = fXMax - fXMin;
-            float fXPart1 = v3RigidBody1Max.X - v3RigidBody1Min.X;
-            float fXPart2 = v3RigidBody2Max.X - v3RigidBody2Min.X;
-            if ( (fXPart1 + fXPart2 + margin) > fFull ) { bCollX = true; }
-
-            // Y
-            bool bCollY = false;
-            float fYMin = Math.Min(v3RigidBody1Min.Y, v3RigidBody2Min.Y);
-            float fYMax = Math.Max(v3RigidBody1Max.Y, v3RigidBody2Max.Y);
-            fFull = fYMax - fYMin;
-            float fYPart1 = v3RigidBody1Max.Y - v3RigidBody1Min.Y;
-            float fYPart2 = v3RigidBody2Max.Y - v3RigidBody2Min.Y;
-            if ((fYPart1 + fYPart2 + margin) > fFull) { bCollY = true; }
-
-            // Z
-            bool bCollZ = false;
-            float fZMin = Math.Min(v3RigidBody1Min.Z, v3RigidBody2Min.Z);
-            float fZMax = Math.Max(v3RigidBody1Max.Z, v3RigidBody2Max.Z);
-            fFull = fZMax - fZMin;
-            float fZPart1 = v3RigidBody1Max.Z - v3RigidBody1Min.Z;
-            float fZPart2 = v3RigidBody2Max.Z - v3RigidBody2Min.Z;
-            if ((fZPart1 + fZPart2 + margin) > fFull) { bCollZ = true; }
-
-            return (bCollX && bCollY && bCollZ);
         }
 
         private static void GetPointsInConvexMesh(RigidBody rigidBody1, RigidBody rigidBody2, ref List<Hit> listHits)
         {
             Matrix4 m4FinalTransform = Matrix4.Mult(rigidBody2.m_m4World, rigidBody1.m_m4World.Inverted());
 
-            for (int id = 0; id < rigidBody2.m_listIndices.Count(); id += 3)
+            int nIdN = 0;
+            for (int id = 0; id < rigidBody2.m_listIndices.Count(); id += 3, nIdN++)
             {
                 Vector3 v3A = rigidBody2.m_listPoints[rigidBody2.m_listIndices[id + 0]];
                 Vector3 v3B = rigidBody2.m_listPoints[rigidBody2.m_listIndices[id + 1]];
                 Vector3 v3C = rigidBody2.m_listPoints[rigidBody2.m_listIndices[id + 2]];
-                Vector3 v3Normal = Vector3.Cross(v3B - v3A, v3C - v3A).Normalized();
+                //Vector3 v3Normal = Vector3.Cross(v3B - v3A, v3C - v3A).Normalized();
+
+                Vector3 v3Normal = rigidBody2.m_listTriangleNormals[nIdN];
 
                 for (int i = 0; i <= step; i++)
                 {
